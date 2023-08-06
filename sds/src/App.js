000bb337b8cc7fc94b8a003d2dc0a9273fcaf10c
cccff12 <ksun0430@naver.com>
2023-08-06 16:46:08 +0900

    ds

---
+++ sds/src/App.js
@@ -0,0 +1,15 @@
+import "./css/App.css";
+import Main from "./comps/ProMain";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>css</h1>
+      </header>
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ sds/src/comps/ProMain.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import "../css/main.css";
+
+const ProMain = () => {
+  const [inputValue1, setInputValue1] = useState(0);
+  const [inputValue2, setInputValue2] = useState(0);
+
+  const handleInputChange1 = (event) => {
+    const newValue = parseInt(event.target.value, 10);
+    setInputValue1(newValue);
+  };
+
+  const handleInputChange2 = (event) => {
+    const newValue = parseInt(event.target.value, 10);
+    setInputValue2(newValue);
+  };
+
+  // 첫 번째 입력값을 넘는지 확인하는 함수
+  const isFirstInputExceeded = inputValue2 > inputValue1;
+
+  return (
+    <>
+      <input type="number" value={inputValue1} onChange={handleInputChange1} />
+      <input type="number" value={inputValue2} onChange={handleInputChange2} />
+      <div className="bar-graph">
+        <div
+          className="bar"
+          style={{
+            height: `${inputValue2}px`,
+            backgroundColor: isFirstInputExceeded ? "red" : "#007bff",
+          }}
+        ></div>
+      </div>
+    </>
+  );
+};
+
+export default ProMain;
